aa98467ef0f2d34c1c75bd0313af5a49078bef51
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-09-02 12:11:50 -0500

    version inicial de la lectura de los datos y estructuracion

--- app/main.go
+++ app/main.go
@@ -1,25 +1,37 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"net/mail"
 	"os"
+	"sync"
 )
 
-// creamos una estructura para almacenar los datos
+var emails = make(chan string, 500)
+var dataToZinc = make(chan Email, 500)
+
+var waG sync.WaitGroup
+
+// create a struct to store the data
 type Email struct {
-	messageId string
-	date      string
-	from      string
-	to        string
-	subject   string
-	body      string
+	ID       string
+	MessageId string
+	Date      string
+	From      string
+	To        string
+	Subject   string
+	Body      string
 }
 
+
 func main() {
-	//iniciar servidor de profiling en un goroutine
+	//start profiling
 	go func() {
 		log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
 	}()
@@ -29,39 +41,111 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Llamar a la función recursiva
-	findsDir(rootDir)
+	go processEmails()
 
-	// Cerrar el directorio raíz
-	// Forzar a vaciar el búfer de salida
+	findsDir(rootDir)
+	close(emails)
+
 	os.Stdout.Sync()
 
-	// Bucle infinito para mantener el programa en ejecución
+	// loop infinitely
 	select {}
 }
 
 func findsDir(dir *os.File) {
-	// Leer el directorio raíz
-	fileInfos, err := dir.Readdir(-1)
+	// Recursively searches for files in a given directory and its subdirectories, and sends the file paths to a channel for further processing.
+
+	// Inputs:
+	// - dir: A pointer to an os.File representing the directory to search.
+
+	// Outputs:
+	// - None.
+	files, err := dir.Readdir(-1)
 	if err != nil {
 		log.Fatal(err)
 	}
+	_ = dir.Close()
 
-	// Recorrer los ficheros del directorio raíz
-	for _, fi := range fileInfos {
-		// Comprobar si es un directorio
-		if fi.IsDir() {
-			// Si es un directorio, abrirlo
-			subDir, err := os.Open(dir.Name() + "/" + fi.Name())
+	total := 0
+	// Iterate over the files in the directory
+	for _, file := range files {
+		// process each file or directory
+		if file.IsDir() {
+			// if the file is a directory, open it and call the function recursively
+			subDir, err := os.Open(dir.Name() + "/" + file.Name())
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			// Llamar recursivamente a la función
 			findsDir(subDir)
 		} else {
-			// Si no es un directorio, imprimir el nombre
-			fmt.Println(dir.Name() + "/" + fi.Name())
+
+			total++
+			waG.Add(1)
+			emails <- dir.Name() + "/" + file.Name()
+
 		}
 	}
+
+}
+
+func processEmails() {
+	// processEmails processes emails from the 'emails' channel.
+	// It reads each email from the channel, parses it using the 'parseEmail' function,
+	// and sends the parsed email data to the 'dataToZinc' channel.
+	// The function also keeps track of the number of processed emails using the 'times' variable.
+	times := 0
+
+	for email := range emails {
+		times++
+		waG.Done()
+		dataToZinc <- parseEmail(email)
+	}
+}
+
+func parseEmail(email string) Email {
+	/*
+		parseEmail parses an email message and extracts relevant information such as the message ID, date, sender, recipient, subject, and body.
+
+		Parameters:
+		- email (string): The email message to be parsed.
+
+		Returns:
+		- Email: Parsed Email struct containing the extracted information from the email message.
+	*/
+
+	fileInfo, err := os.ReadFile(email)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msg, err := mail.ReadMessage(bytes.NewReader(fileInfo))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Parse the message
+	header := msg.Header
+	body, err := io.ReadAll(msg.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a struct to store the data
+	emailData := Email{
+		ID:       email,
+		MessageId: header.Get("Message-Id"),
+		Date:      header.Get("Date"),
+		From:      header.Get("From"),
+		To:        header.Get("To"),
+		Subject:   header.Get("Subject"),
+		Body:      string(body),
+	}
+
+	jsonData, err := json.Marshal(emailData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(jsonData))
+	return emailData
 }
